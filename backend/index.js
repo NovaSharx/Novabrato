dc8b5ff2ff25a4c3324a640a4ac139fba119b4e8
Davin <davin-dj@hotmail.com>
2023-04-17 10:43:57 -0400

    Modified for deployment.

--- backend/index.js
+++ backend/index.js
@@ -4,20 +4,20 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const defineCurrentUser = require('./middleware/defineCurrentUser')
 const app = express()
-const path = require('path');
+// const path = require('path');
 require('dotenv').config()
 
 // Express Settings
 app.use(cors())
-app.use(express.static('public'))
+// app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(defineCurrentUser)
 
 // serve static front end in production mode
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, 'client', 'build')));
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, 'client', 'build')));
+// }
 
 // Routes
 app.get('/', async (req, res) => {
