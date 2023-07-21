dc41c8643ebb88138d5d75856e9b1e6903ca16c7
Davin <davin-dj@hotmail.com>
2023-07-21 15:03:30 -0400

    Added initial layout for exercise category selection.

--- frontend/src/components/Exercises/Exercises.tsx
+++ frontend/src/components/Exercises/Exercises.tsx
@@ -1,9 +1,20 @@
 import { FC, ReactElement, useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import styled from '@emotion/styled';
 
 const Exercises: FC = (): ReactElement => {
 
     const { theme } = useContext(ThemeContext)!
+
+    const CategoryButton = styled.span`
+        background-color: ${theme.palette.background.primary};
+
+        &:hover {
+            color: white;
+            background-color: ${theme.palette.secondary.main};
+            box-shadow: 0rem 0rem 2rem 0rem ${theme.palette.shadow.primary};
+        }
+    `;
 
     return (
         <div id='exercises' style={{
@@ -11,9 +22,29 @@ const Exercises: FC = (): ReactElement => {
         }}>
 
             <div className='main-section-container'>
-                <div className='main-section'>
+                <div className='main-section' style={{ color: theme.palette.text.primary }}>
 
-                    Exercises
+                    <h2 style={{ borderBottom: `2px solid ${theme.palette.divider.tertiary}` }}>
+                        EXERCISES
+                    </h2>
+
+                    <div id='exercise-selection'>
+
+                        <p id='exercise-selection-prompt' style={{ color: theme.palette.text.secondary }}>Please choose a skill to practice.</p>
+
+                        <div id='exercise-categories'>
+
+                            <CategoryButton className='exercise-category'>
+                                <span>Ear Training</span>
+                            </CategoryButton>
+
+                            <CategoryButton className='exercise-category'>
+                                <span>Theory & Knowledge</span>
+                            </CategoryButton>
+
+                        </div>
+
+                    </div>
 
                 </div>
             </div>
