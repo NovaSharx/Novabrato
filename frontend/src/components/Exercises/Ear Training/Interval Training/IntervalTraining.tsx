4d2262f8aa0617dcd0b5a5682c3a9d29d89e8eb6
Davin <davin-dj@hotmail.com>
2023-08-03 22:16:06 -0400

    Created basic interval range selection interface for interval training exercise.

--- frontend/src/components/Exercises/Ear Training/Interval Training/IntervalTraining.tsx
+++ frontend/src/components/Exercises/Ear Training/Interval Training/IntervalTraining.tsx
@@ -1,9 +1,42 @@
-import { FC, Fragment, ReactElement, useContext } from 'react';
+import { FC, Fragment, ReactElement, useContext, useState } from 'react';
 import { ThemeContext } from '../../../contexts/ThemeContext';
+import styled from '@emotion/styled';
 
 const IntervalTraining: FC = (): ReactElement => {
 
     const { theme } = useContext(ThemeContext)!
+
+    const [intervalRange, setIntervalRange] = useState<number>(3);
+
+    function changeIntervalRange(amount: number): void {
+        let updatedIntervalRange: number = intervalRange + amount;
+
+        if (updatedIntervalRange < 1 || updatedIntervalRange > 12) {
+            return;
+        } else {
+            setIntervalRange(updatedIntervalRange);
+        }
+    }
+
+    const IntervalRangeControlButton = styled.span`
+        background-color: transparent;
+
+        &:hover {
+            color: ${theme.palette.primary.main};
+            background-color: ${theme.palette.background.primary};
+        }
+
+    `
+
+    const StartButton = styled.span`
+        border-color: transparent;
+        background-color: ${theme.palette.background.full};
+
+        &:hover {
+            border-color: ${theme.palette.primary.main};
+            background-color: ${theme.palette.background.secondary};
+        }
+    `
 
     return (
         <Fragment>
@@ -14,6 +47,20 @@ const IntervalTraining: FC = (): ReactElement => {
 
             <div id='exercise-content'>
 
+                <p id='exercise-content-prompt' style={{ color: theme.palette.text.secondary }}>Please select an interval range.</p>
+
+                <div id='interval-range-selection-window'>
+
+                    <div id='interval-range-controls'>
+                        <IntervalRangeControlButton className='interval-range-control-button' style={{ color: intervalRange <= 1 ? theme.palette.text.disabled : 'inherit' }} onClick={() => changeIntervalRange(-1)}>-</IntervalRangeControlButton>
+                        <span id='interval-range-display'>{intervalRange}</span>
+                        <IntervalRangeControlButton className='interval-range-control-button' style={{ color: intervalRange >= 12 ? theme.palette.text.disabled : 'inherit' }} onClick={() => changeIntervalRange(1)}>+</IntervalRangeControlButton>
+                    </div>
+
+                    <span id='interval-range-unit'>Half Step(s)</span>
+
+                    <StartButton id='interval-range-start-button'>START</StartButton>
+                </div>
             </div>
 
         </Fragment>
