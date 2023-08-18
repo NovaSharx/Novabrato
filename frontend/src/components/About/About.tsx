32fd1fd0846e7dafb8e5083a2f82c1f80f935f1f
Davin <davin-dj@hotmail.com>
2023-08-18 14:34:03 -0400

    Corrected grammatical error.

--- frontend/src/components/About/About.tsx
+++ frontend/src/components/About/About.tsx
@@ -24,7 +24,7 @@ const About: FC = (): ReactElement => {
                             <br />
                             <p>When I started my journey as a beginner guitarist I wished that I had a convenient way of visualizing the notes of the fretboard.</p>
                             <br />
-                            <p>I struggled to understanding certain music theory concepts and how they all related to each other.</p>
+                            <p>I struggled to understanding certain music theory concepts and how they all relate to each other.</p>
                             <br />
                             <p>My <span>goal</span> is to give beginner guitar players that push to <span>not give up</span> and to continue to learn and <span>persevere</span> during this crucial stage in their journey.</p>
                             <br />
